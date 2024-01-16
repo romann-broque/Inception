82df4679cf76f1d5b3da18d05f92a5c638742f8b
romann-broque <kinoide56@gmail.com>
2024-01-16 14:32:28 +0000

    commands for mariadb are done for now

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -3,17 +3,14 @@
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 
-# Initialize the database if it doesn't exist
-# if [ -d "/var/lib/mysql/mysql" ]; then
-#     echo "Database already exists"
-# else
-	# Create the database and set up permissions
-    echo "Creating database"
-	tfile="$(mktemp)"
-	if [ ! -f "$tfile" ]; then
-		return 1
-	fi
-	cat << EOF > $tfile
+echo "Creating database"
+tfile="$(mktemp)"
+if [ ! -f "$tfile" ]; then
+	return 1
+fi
+
+# Store commands into a temporary file
+cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;
@@ -23,17 +20,14 @@ DROP DATABASE IF EXISTS test;
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO 'user'@'%';
 FLUSH PRIVILEGES;
 EOF
 
-	# echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$tfile"
-	# echo "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" >> "$tfile"
-	# echo "FLUSH PRIVILEGES;" >> "$tfile";
-	/usr/sbin/mysqld --user=mysql --bootstrap --verbose --skip-name-resolve --skip-networking=0 < "$tfile"
-	echo $?
-	cat $tfile
-	rm -f "$tfile"
-# fi
+# Initialize mysqld
+/usr/sbin/mysqld --user=mysql --bootstrap --verbose --skip-name-resolve --skip-networking=0 < "$tfile"
+cat $tfile
+rm -f "$tfile"
 
 # Keep the MariaDB server running in the foreground
 exec /usr/sbin/mysqld --user=mysql --console --skip-name-resolve --skip-networking=0 $@
